d6f3b22a8e0ecbca80a24fa01bfc1113e5741383
Darshu236 <151632000+Darshu236@users.noreply.github.com>
2025-03-21 08:41:27 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, Jenkins Webhook is working!" << endl;
-cout << "Jenkins CI/CD Pipeline Test" << endl;
+
   return 0;
 }
 
